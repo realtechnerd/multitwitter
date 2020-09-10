3492de0bb4909ff06625decf60d238ee16e1e983
realtechnerd <40891074+realtechnerd@users.noreply.github.com>
2020-09-10 13:00:34 -0400

    Updates

---
+++ precache-manifest.2b9cb5e595d6410229b6ce83e2c6f51d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aebf065927f4502c6ba991ae5f46260a",
+    "url": "/multitwitter/index.html"
+  },
+  {
+    "revision": "520f788f8d1bed684cff",
+    "url": "/multitwitter/static/css/main.83435366.chunk.css"
+  },
+  {
+    "revision": "3ae88a82e68bbe176e80",
+    "url": "/multitwitter/static/js/2.3dfaffa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/multitwitter/static/js/2.3dfaffa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520f788f8d1bed684cff",
+    "url": "/multitwitter/static/js/main.d5b4db3f.chunk.js"
+  },
+  {
+    "revision": "43e2aa045818fd4be5a4",
+    "url": "/multitwitter/static/js/runtime-main.5fe82d37.js"
+  }
+]);
